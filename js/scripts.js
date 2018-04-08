b0bbe451a711b7d3ffb33ddca0b1662a2c76c72a
Dmitri Prozorov <di.prozorov@gmail.com>
2018-04-08 18:53:11 +0300

    допилино
    по замечаниям:
    Б9 - исправлено как в стайлгайде
    Б11 - партнеры и каталог по пикселю
    Б12 - в ие футер - ок.
    Б15 - кавычки, все на месте. html css js пропущены через форматер
    Д9 - указаны все префексы. код пропущен через автопрефиксер.
    Д11 - все по стайлгайду
    Д15-17 - ОК
    Д18 - карта отключена, тк с помощью iframe настроек мало и выглядит это в итоге убого в сочетании с блоком контактов и меп-маркером.
    Д23 - ок.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 var contactsBtn = document.querySelector(".contacts-btn");
 var popup = document.querySelector(".modal");
-  
+
+var close = popup.querySelector(".modal-close");
+
 contactsBtn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-show");
-  });
+   evt.preventDefault();
+   popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+         popup.classList.remove("modal-show");
+      }
+   }
+});
